f1d0a1ca25ea236c1ff5eb30be0612c773ae4f0e
Andrei Bratu <bratuandrei0@gmail.com>
2020-04-08 14:24:33 +0300

    Implemented AddConferenceModal UI

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,7 @@ import * as serviceWorker from './serviceWorker';
 import {Client as Styletron} from 'styletron-engine-atomic';
 import {Provider as StyletronProvider} from 'styletron-react';
 import {LightTheme, BaseProvider, styled} from 'baseui';
+import ModalDemo from "./components/ModalDemo/ModalDemo";
 
 
 const engine = new Styletron();
@@ -21,7 +22,7 @@ ReactDOM.render(
         <BaseProvider theme={LightTheme}>
           {/*  Application entry point */}
           <Centered>
-            <p>Hello world!</p>
+            <ModalDemo/>
           </Centered>
         </BaseProvider>
       </StyletronProvider>
