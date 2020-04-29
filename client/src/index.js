8618bd574388827f0c1a0b7f98023ebd5a943860
Andrei Bratu <bratuandrei0@gmail.com>
2020-04-29 13:16:45 +0300

    Amended add conference modal with scm and pcm

--- client/src/index.js
+++ client/src/index.js
@@ -7,26 +7,26 @@ import {Provider as StyletronProvider} from 'styletron-react';
 import {LightTheme, BaseProvider} from 'baseui';
 import {Provider} from "react-redux";
 import store from './store.js'
-import Evaluation from "./components/Evaluation/Evaluation";
 import Navbar from "./components/Navbar/Navbar";
+import ConferenceModalDemo from "./components/ConferenceModalDemo/ConferenceModalDemo";
 
 const engine = new Styletron();
-const colorScale = {
-    '-3': '#ff0000',
-    '-2': '#fb5600',
-    '-1': '#ee8200',
-    '0' : '#d7a700',
-    '1' : '#b6c700',
-    '2' : '#86e400',
-    '3' : '#00ff00'
-};
+// const colorScale = {
+//     '-3': '#ff0000',
+//     '-2': '#fb5600',
+//     '-1': '#ee8200',
+//     '0' : '#d7a700',
+//     '1' : '#b6c700',
+//     '2' : '#86e400',
+//     '3' : '#00ff00'
+// };
 
 ReactDOM.render(
     <Provider store={store}>
         <React.StrictMode>
           <StyletronProvider value={engine}>
             <BaseProvider theme={LightTheme}>
-                <Navbar/>
+                <ConferenceModalDemo/>
             </BaseProvider>
           </StyletronProvider>
         </React.StrictMode>
